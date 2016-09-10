a30f641082968d4b42ddcd893d87eb178d62302a
Sijomonr <sijo.r@orisys.in>
2016-09-10 12:02:15 +0530

    Add files via upload

---
+++ mlt/index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MyLandlordTenant</title>    
+        <link rel="stylesheet" href="fa/css/font-awesome.min.css"/>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+        <a class="navbar-brand" href="#"><img src="images/logo.jpg" alt="logo"/></a>
+    </div>
+  </div>
+</nav>
+        <section id="topimg">
+            <div class="col-md-12 text-center">
+                <h2>Simple, easier and efficient <br> consultation methods</h2>
+                <a href="#">LOGIN</a>
+                <a href="#">REGISTER</a>
+            </div>
+        </section> 
+        <section id="banner"></section>
+     <section id="ics" class="text-center">
+    <div class="row">
+        <div class="container">
+            <div class="col-md-5ths">
+                <div><img src="images/1.png" alt="1a"/></div>
+                <div>IT CONSULTING</div>
+            </div>
+            <div class="col-md-5ths">
+                 <div><img src="images/2.png" alt="2a"/></div>
+                  <div>DATA ANALYTICS</div>
+            </div>
+            <div class="col-md-5ths">
+                  <div> <img src="images/3.png" alt="3a"/></div>
+                   <div>CTI INTEGRATIONS</div>
+            </div>
+            <div class="col-md-5ths">
+                   <div><img src="images/4.png" alt="4a"/></div>
+                   <div>MOBILE APPS</div>
+            </div>
+             <div class="col-md-5ths">
+                   <div><img src="images/5.png" alt="5a"/></div>
+                    <div>CLOUD COMPUTING</div>
+            </div>
+        </div>
+    </div>
+</section>
+        <section id="connect">
+            <div class="container">
+            <div class="col-md-10">
+                <h2>Connecting the angles of Real Estate <br> Infrastructure</h2>
+                <p>printing and typesetting industry. Lorem lpsum has been the industry's standard dummy text ever
+since the lSOOs. when an unknown printer took a galley of type and scrambled it to make a type
+specimen book. It has survived not only five centuries. but leap into electronic typesetting.
+remaining essentially unchangedprinting and typesetting industry. Lorem enthe
+industry's standard dummy text ever since the lSOOs, when an unknown printer took a galley of type
+and scrambled it to make a type specimen book. It has survived not only five centuries. but also the
+leap into electronic typesetting. remaining essentially unchanged.</p>
+            </div>
+                </div>
+        </section>
+          <section id="products">
+              <div class="container">
+                  <h2 class="text-center">Products</h2>
+               <div class="col-md-4">
+                  <div class="prod_img"><img  class="img-responsive" src="images/im1.jpg" alt="p1"/></div>
+                  <div class="prod_desc text-center">
+                      <h4>Lorem Ipsum is simply industry</h4>
+                      <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                          when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                      <a href="#"><img src="images/next.png" alt="next"> Read More</a>
+                  </div>
+              </div>
+                    <div class="col-md-4">
+                  <div class="prod_img"><img class="img-responsive" src="images/im2.jpg" alt="p2"/></div>
+                  <div class="prod_desc text-center">
+                       <h4>Lorem Ipsum is simply industry</h4>
+                      <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                          when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                          <a href="#"><img src="images/next.png" alt="next"> Read More</a>
+                  </div>
+              </div>
+                    <div class="col-md-4">
+                  <div class="prod_img"><img class="img-responsive" src="images/im3.jpg" alt="p3"/></div>
+                  <div class="prod_desc text-center">
+                       <h4>Lorem Ipsum is simply industry</h4>
+                      <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                          when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                         <a href="#"><img src="images/next.png" alt="next"> Read More</a>
+                  </div>
+              </div>
+                  </div>
+        </section>
+        
+        <footer>
+                <div class="container">
+                                <div class="row">
+                <div class="col-md-3">
+                    <ul>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Register</a></li>
+                        <div>
+                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </div>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h3>Products</h3>
+                    <ul>
+                        <li><a href="#">MyREReport</a></li>
+                        <li><a href="#">MyBuildingReport</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                     <h3>Services</h3>
+                    <ul>
+                        <li><a href="#">IT Consulting</a></li>
+                        <li><a href="#">Mobile Development</a></li>
+                    </ul>
+                </div>
+            </div>
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h5>(c) <?php echo date("Y");?>. All right reserved.</h5>
+                        </div>
+                    </div>
+                 </div>
+        </footer>    
+        
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" type="text/javascript"></script>
+       <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
